a149f29d6677602f7b0de517b17e795fb762bc60
amasanelli <36801026+amasanelli@users.noreply.github.com>
2021-07-03 13:53:55 -0300

    test

--- Albor - Comprobante de compra.user.js
+++ Albor - Comprobante de compra.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Albor - Comprobante de compra
-// @version      0.3
+// @version      1.0
 // @namespace    https://github.com/amasanelli/albor-patch
 // @description  Agrega medio de pago a comprobante de compra
 // @author       masanelli.a
