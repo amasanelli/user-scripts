eb3c26fd78269faf5cac13269542c13d243705eb
Andres Masanelli <36801026+amasanelli@users.noreply.github.com>
2022-03-28 16:00:57 -0300

    Update Albor - Comprobante de compra.user.js

--- Albor - Comprobante de compra.user.js
+++ Albor - Comprobante de compra.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Albor - Comprobante de compra
-// @version      1.0
+// @version      2.0
 // @namespace    https://github.com/amasanelli/user-scripts
 // @description  Agrega medio de pago a comprobante de compra
 // @author       masanelli.a
@@ -14,16 +14,21 @@
 
 (function() {
 
-    function parseObs(obs) {
-        var arr = obs.split(' | ');
-        var res = {};
+    const opts = ['APORTE', 'CANJE', 'CH-Fis', 'Echeq', 'TC', 'Transf', 'CH-Fis-Terc', 'Echeq-Terc', 'COMPENSACION', 'MUTUO', 'Con descuento 5%'];
+    var parsedObs = {};
 
-        for (var i=0; i < arr.length; i++) {
-            var element = arr[i];
+    function parseObs(obs) {
+        const arr = obs.split(' | ');
+        const res = {};
+
+        for (let i=0; i < arr.length; i++) {
+            const element = arr[i];
             if (element.startsWith('MDP=')) {
                 res.MDP = element.substring(4);
             } else if (element.startsWith('CMT=')) {
                 res.CMT = element.substring(4);
+            } else if (element.startsWith('RTO=')) {
+                res.RTO = element.substring(4);
             } else {
                 res.CMT = element;
             }
@@ -34,231 +39,268 @@
     }
 
     function updateObs(obs) {
-        var obj = document.getElementById('Observaciones');
-
-        obj.value = 'CMT=' + obs.CMT + ' | MDP=' + obs.MDP;
+        const obj = document.getElementById('Observaciones');
+        obj.value = 'CMT=' + obs.CMT + ' | MDP=' + obs.MDP + ' | RTO=' + obs.RTO;
     }
 
     function getComprobante(id) {
-        var req = new XMLHttpRequest();
+        const req = new XMLHttpRequest();
         req.open('GET', 'https://adblick.alboragro.com/1/Ordenes_Compra/Detail/' + id, false);
         req.send(null);
         if (req.status == 200) {
-            var obs = new Set();
-            var parser = new DOMParser();
-            var responseDoc = parser.parseFromString(req.responseText, 'text/html');
+            const obs = new Set();
+            const parser = new DOMParser();
+            const responseDoc = parser.parseFromString(req.responseText, 'text/html');
             obs.add(responseDoc.getElementById('Observaciones').value);
-            var data = Array.from(obs).join(', ');
+            const data = Array.from(obs).join(', ');
             return data;
         }
     }
 
+    function agregarMedioDePago() {
+        const medio_pct = document.getElementById('medio_pct').value;
+        const medio_len = document.getElementById('medio_len').value;
+        const medio_sel = document.getElementById('medio_sel').value;
+        const mdp = document.getElementById('mdp');
+
+        if (medio_pct == '' || medio_len == '') {
+            alert('Faltan datos')
+            return;
+        }
+        if (mdp.value == 'undefined') {
+            mdp.value = '';
+        }
+        mdp.value += (mdp.value == '' ? '' : ' + ') + medio_pct + ' % ' + medio_sel + ' ' + medio_len + ' d';
+
+        parsedObs.MDP = mdp.value;
+        updateObs(parsedObs);
+    }
+
+    function agregarRemito() {
+        const remito_pre = document.getElementById('remito_pre').value;
+        const remito_suf = document.getElementById('remito_suf').value;
+        const rto = document.getElementById('rto');
+
+        if (remito_pre == '' || remito_suf == '') {
+            alert('Faltan datos')
+            return;
+        }
+        if (rto.value == 'undefined') {
+            rto.value = '';
+        }
+        rto.value += (rto.value == '' ? '' : '; ') + parseInt(remito_pre) + '-' + parseInt(remito_suf);
+
+        parsedObs.RTO = rto.value;
+        updateObs(parsedObs);
+    }
+
+    function verObservaciones() {
+        const tbl = document.getElementById('DetalleGrid');
+
+        const comprobantes = new Set();
+        const rows = [];
+
+        for (let i = 0, row; row = tbl.rows[i]; i++) {
+            for (let j = 0, col; col = row.cells[j]; j++) {
+                if (col.getAttribute('aria-describedby') == 'DetalleGrid_ItemVinculable') {
+                    let json = col.title;
+                    try {
+                        json = json.replace('_TERRAJSON_','').replaceAll("'",'"');
+                        json = JSON.parse(json);
+                        let id = json.ID_Comprobante;
+                        comprobantes.add(id);
+                        rows.push([id,'']);
+                    } catch (err) {
+                        console.log('Item sin detalle item vinculable');
+                    }
+                    break;
+                }
+            }
+        }
+
+        for (let it = comprobantes.values(), comprobante; comprobante=it.next().value;) {
+            let obs = getComprobante(comprobante);
+            rows.forEach(element => {
+                if (element[0] == comprobante) {
+                    element[1] = obs;
+                }
+            })
+        }
+
+        const head = document.getElementById('jqgh_DetalleGrid_Unidad_Medida');
+        head.innerHTML = 'Observaciones';
+
+        if (rows.length == 0) { return }
+
+        for (let i = 1, row; row = tbl.rows[i]; i++) {
+            for (let j = 0, col; col = row.cells[j]; j++) {
+                if (col.getAttribute('aria-describedby') == 'DetalleGrid_Unidad_Medida') {
+                    col.innerHTML = rows[i - 1][1];
+                }
+            }
+        }
+    }
+
+    function verObservacionsGrid() {
+        const tbl = document.getElementById('GridVinculacionesPosibles');
+
+        const head = document.getElementById('jqgh_GridVinculacionesPosibles_Numero_Comprobante');
+        head.innerHTML = 'Observaciones';
+
+        const rows = [];
+
+        for (let i = 0, row; row = tbl.rows[i]; i++) {
+            for (let j = 0, col; col = row.cells[j]; j++) {
+                if (col.getAttribute('aria-describedby') == 'GridVinculacionesPosibles_Comprobante') {
+                    console.log(col.title);
+                    rows.push(getComprobante(col.title));
+                }
+            }
+        }
+
+        for (let i = 0, row; row = tbl.rows[i]; i++) {
+            for (let j = 0, col; col = row.cells[j]; j++) {
+                if (col.getAttribute('aria-describedby') == 'GridVinculacionesPosibles_Numero_Comprobante') {
+                    col.innerHTML = rows[i];
+                }
+            }
+        }
+    }
+
     function addPatch() {
-        var obs = document.getElementById('Observaciones');
+        const obs = document.getElementById('Observaciones');
         obs.readOnly = true;
         obs.style.backgroundColor = '#f2f2f2';
 
-        var parsedObs = parseObs(obs.value);
+        parsedObs = parseObs(obs.value);
 
-        var frame0 = document.getElementById('grales-cbte-cpra');
-        var patch = document.createElement('li');
+        const frame0 = document.getElementById('grales-cbte-cpra');
 
-        var label0 = document.createElement('label');
-        label0.class = 'control-label';
-        label0.appendChild(document.createTextNode('Comentarios'));
+        const comentarios = document.createElement('li');
+        comentarios.innerHTML = `
+        <label class="control-label">Comentarios</label>
+        <span class="inline-labels">
+           <textarea id="cmt" class="form-control"></textarea>
+        </span>
+        `
 
-        var cmt = document.createElement('textarea');
-        cmt.style.marginBottom = '30px';
+        const medios_de_pago = document.createElement('li');
+        medios_de_pago.innerHTML = `
+        <label class="control-label">Medio de pago</label>
+        <span class="inline-labels">
+           <textarea id="mdp" class="form-control"></textarea>
+        </span>
+        `
+
+        const medio_de_pago = document.createElement('li');
+        medio_de_pago.innerHTML = `
+        <label class="control-label">Agregar medio</label>
+        <span class="inline-labels">
+           <input id="medio_pct" class="form-control" type="number">
+           <span class="control-label"> %</span>
+           <select id="medio_sel" class="form-control"></select>
+           <input id="medio_len" class="form-control" type="number">
+           <span class="control-label"> d</span>
+           <input id="medio_btn" type="button" value="Agregar">
+        </span>
+        `
+
+        const remitos = document.createElement('li');
+        remitos.innerHTML = `
+        <label class="control-label">Remitos</label>
+        <span class="inline-labels">
+           <textarea id="rto" class="form-control"></textarea>
+        </span>
+        `;
+
+        const remito = document.createElement('li');
+        remito.innerHTML = `
+        <label class="control-label">Agregar remito</label>
+        <span class="inline-labels">
+           <input id="remito_pre" class="form-control" type="number">
+           <span class="control-label"> - </span>
+           <input id="remito_suf" class="form-control" type="number">
+           <input id="remito_btn" type="button" value="Agregar">
+        </span>
+        `
+
+        frame0.appendChild(comentarios);
+        frame0.appendChild(medios_de_pago);
+        frame0.appendChild(medio_de_pago);
+        frame0.appendChild(remitos);
+        frame0.appendChild(remito);
+
+        const cmt = document.getElementById('cmt');
         cmt.value = parsedObs.CMT;
         cmt.onchange = function() {
             parsedObs.CMT = cmt.value;
             updateObs(parsedObs);
         }
 
-        var br0 = document.createElement('br');
-
-        var label1 = document.createElement('label');
-        label1.class = 'control-label';
-        label1.appendChild(document.createTextNode('Medio de pago'));
-
-        var mdp = document.createElement('textarea');
-        mdp.style.marginBottom = '30px';
+        const mdp = document.getElementById('mdp');
         mdp.value = parsedObs.MDP;
         mdp.onchange = function() {
             parsedObs.MDP = mdp.value;
             updateObs(parsedObs);
         }
 
-        var br1 = document.createElement('br');
-
-        var label2 = document.createElement('label');
-        label2.class = 'control-label';
-        label2.appendChild(document.createTextNode('Agregar medio'));
-
-        var pct = document.createElement('input');
-        pct.classList.add('form-control');
-        pct.type = 'number';
-        pct.value = 100;
-
-        var label3 = document.createElement('span');
-        label3.appendChild(document.createTextNode(' %'));
-        label3.style.marginRight = '10px';
-
-        var select = document.createElement('select');
-
-        var opts = ['APORTE', 'CANJE', 'CH-Fis', 'Echeq', 'TC', 'Transf', 'CH-Fis-Terc', 'Echeq-Terc', 'COMPENSACION', 'MUTUO', 'Con descuento 5%'];
-        for (var i=0; i < opts.length; i++) {
-            let opt = document.createElement('option');
+        const medio_sel = document.getElementById('medio_sel');
+        for (let i=0; i < opts.length; i++) {
+            const opt = document.createElement('option');
             opt.appendChild(document.createTextNode(opts[i]));
-            select.appendChild(opt);
+            medio_sel.appendChild(opt);
         }
-        select.style.marginRight = '10px';
-        select.value = 'Transf';
+        medio_sel.value = 'Transf';
 
-        var d = document.createElement('input');
-        d.classList.add('form-control');
-        d.type = 'number';
-        d.value = 30;
+        const medio_btn = document.getElementById('medio_btn');
+        medio_btn.addEventListener('click', agregarMedioDePago);
 
-        var label4 = document.createElement('span');
-        label4.appendChild(document.createTextNode(' d'));
-        label4.style.marginRight = '10px';
-
-        var btn0 = document.createElement('span');
-        btn0.style.border = '1px solid #000000';
-        btn0.style.padding = '5px';
-        btn0.style.backgroundColor = '#cccccc';
-        btn0.style.cursor = 'pointer';
-        btn0.appendChild(document.createTextNode('Agregar'));
-        btn0.onclick = function() {
-            if (pct.value == '' || d.value == '') {
-                alert('Faltan datos')
-                return;
-            }
-            if (mdp.value == 'undefined') {
-                mdp.value = '';
-            }
-            mdp.value += (mdp.value == '' ? '' : ' + ') + pct.value + ' % ' + select.value + ' ' + d.value + ' d';
-
-            parsedObs.MDP = mdp.value;
+        const rto = document.getElementById('rto');
+        rto.value = parsedObs.RTO;
+        rto.onchange = function() {
+            parsedObs.RTO = rto.value;
             updateObs(parsedObs);
         }
 
-        patch.appendChild(label0);
-        patch.appendChild(cmt);
-        patch.appendChild(br0);
+        const remito_btn = document.getElementById('remito_btn');
+        remito_btn.addEventListener('click', agregarRemito);
 
-        patch.appendChild(label1);
-        patch.appendChild(mdp);
-        patch.appendChild(br1);
 
-        patch.appendChild(label2);
-        patch.appendChild(pct);
-        patch.appendChild(label3);
-        patch.appendChild(select);
-        patch.appendChild(d);
-        patch.appendChild(label4);
-        patch.appendChild(btn0);
+        /*
+        */
 
-        frame0.appendChild(patch);
+        const frame1 = document.getElementById('opciones-vinculaciones');
 
-        var frame1 = document.getElementById('opciones-vinculaciones');
+        const ops_obs = document.createElement('span');
+        ops_obs.innerHTML = `
+        <input type="button" value="Ver obs" id="obs_btn" style="font-size: 11px; padding: 2px 8px;">
+        `;
 
-        var btn1 = document.createElement('span');
-        btn1.style.border = '1px solid #000000';
-        btn1.style.padding = '5px';
-        btn1.style.backgroundColor = '#cccccc';
-        btn1.style.cursor = 'pointer';
-        btn1.appendChild(document.createTextNode('Ver obs'));
-        btn1.onclick = function() {
-            var tbl = document.getElementById('DetalleGrid');
+        frame1.appendChild(ops_obs);
 
-            var comprobantes = new Set();
-            var rows = [];
+        const obs_btn = document.getElementById('obs_btn');
+        obs_btn.addEventListener('click', verObservaciones);
 
-            for (let i = 0, row; row = tbl.rows[i]; i++) {
-                for (let j = 0, col; col = row.cells[j]; j++) {
-                    if (col.getAttribute('aria-describedby') == 'DetalleGrid_ItemVinculable') {
-                        let json = col.title;
-                        try {
-                            json = json.replace('_TERRAJSON_','').replaceAll("'",'"');
-                            json = JSON.parse(json);
-                            let id = json.ID_Comprobante;
-                            comprobantes.add(id);
-                            rows.push([id,'']);
-                        } catch (err) {
-                            console.log('Item sin detalle item vinculable');
-                        }
-                        break;
-                    }
-                }
-            }
 
-            for (let it = comprobantes.values(), comprobante; comprobante=it.next().value;) {
-                let obs = getComprobante(comprobante);
-                rows.forEach(element => {
-                    if (element[0] == comprobante) {
-                        element[1] = obs;
-                    }
-                })
-            }
-
-            var head = document.getElementById('jqgh_DetalleGrid_Unidad_Medida');
-            head.innerHTML = 'Observaciones';
-
-            if (rows.length == 0) { return }
-
-            for (let i = 1, row; row = tbl.rows[i]; i++) {
-                for (let j = 0, col; col = row.cells[j]; j++) {
-                    if (col.getAttribute('aria-describedby') == 'DetalleGrid_Unidad_Medida') {
-                        col.innerHTML = rows[i - 1][1];
-                    }
-                }
-            }
-        }
-
-        frame1.appendChild(btn1);
+        /*
+        */
 
         window.setInterval(function() {
-            var frame2 = document.getElementById('dialog-vinculaciones');
+            const frame2 = document.getElementById('dialog-vinculaciones');
 
-            var visible = frame2.innerHTML.includes('<') && frame2.parentNode.style.display != 'none';
+            const visible = frame2.innerHTML.includes('<') && frame2.parentNode.style.display != 'none';
 
             if (visible && !visible_old) {
-                var btn2 = document.createElement('span');
 
-                btn2.style.border = '1px solid #000000';
-                btn2.style.padding = '5px';
-                btn2.style.backgroundColor = '#cccccc';
-                btn2.style.cursor = 'pointer';
-                btn2.appendChild(document.createTextNode('Ver obs'));
-                btn2.onclick = function() {
-                    var tbl = document.getElementById('GridVinculacionesPosibles');
+                const ops_obs = document.createElement('span');
+                ops_obs.innerHTML = `
+                <input type="button" value="Ver obs" id="obs_grid_btn" style="font-size: 11px; padding: 2px 8px;">
+                `;
 
-                    var head = document.getElementById('jqgh_GridVinculacionesPosibles_Numero_Comprobante');
-                    head.innerHTML = 'Observaciones';
+                frame2.appendChild(ops_obs);
 
-                    var rows = [];
-
-                    for (let i = 0, row; row = tbl.rows[i]; i++) {
-                        for (let j = 0, col; col = row.cells[j]; j++) {
-                            if (col.getAttribute('aria-describedby') == 'GridVinculacionesPosibles_Comprobante') {
-                                console.log(col.title);
-                                rows.push(getComprobante(col.title));
-                            }
-                        }
-                    }
-
-                    for (let i = 0, row; row = tbl.rows[i]; i++) {
-                        for (let j = 0, col; col = row.cells[j]; j++) {
-                            if (col.getAttribute('aria-describedby') == 'GridVinculacionesPosibles_Numero_Comprobante') {
-                                col.innerHTML = rows[i];
-                            }
-                        }
-                    }
-                }
-
-                frame2.appendChild(btn2);
+                const obs_btn = document.getElementById('obs_grid_btn');
+                obs_btn.addEventListener('click', verObservacionsGrid);
             }
 
             visible_old = visible;
@@ -268,7 +310,7 @@
 
     var visible_old = false;
 
-    var tipo = document.getElementById('ID_Tipo_Comprobante');
+    const tipo = document.getElementById('ID_Tipo_Comprobante');
     if (!tipo) { return }
 
     if (tipo.type == 'hidden') {
